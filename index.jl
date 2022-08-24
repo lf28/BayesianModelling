f71451752c2b2936f131045ccba95ef95b71105c
Lei <lei.fang.87@gmail.com>
2022-08-24 22:36:35 +0100

    Create index.jl

---
+++ index.jl
@@ -0,0 +1,46 @@
+### A Pluto.jl notebook ###
+# v0.19.11
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4393c278-23f4-11ed-13f8-0d5e1856794f
+md"""
+
+# A course on Bayesian modelling
+
+##### Lei Fang
+
+##### University of St Andrews
+"""
+
+# ╔═╡ 3defd8e1-b7eb-4bfe-ba8b-f01ff34de41d
+md"""
+
+## Topics
+
+1. [Introduction to Julia](https://lf28.github.io/BayesianML/MLE.html)
+2. [MCMC](./section3_mcmc.html)
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.0"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─4393c278-23f4-11ed-13f8-0d5e1856794f
+# ╟─3defd8e1-b7eb-4bfe-ba8b-f01ff34de41d
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
